5868735b2e0df1721c76ebfaa5ad822add67fbe6
KiRaHg0 <a19203263@alumnos.uady.mx>
2024-08-07 12:11:17 -0600

    Clase 17

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let app = document.getElementById('app');
 
-let typewriter = new Typewriter(app,{
-    loop:true,
-    delay:75,
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
 })
 
 // reconoces una acción porque tiene paréntesis después del nombre de la acción
@@ -15,3 +15,29 @@ typewriter
     .typeString('Desarrolladora FRONTEND JR')
     .pauseFor(1000)
     .start();
+
+
+
+let audioElement; //variable para almacenar objeto de audio
+
+const mainElement = document.querySelector('main'); // seleccionar la etiqueta main
+
+mainElement.addEventListener('click', function () {
+    if (!audioElement) {
+        audioElement = new Audio('assets/music/we.mp3');
+        audioElement.volume = 0.1
+    }
+    audioElement.play();
+});
+
+const pauseButton = document.getElementById('pauseButton');
+
+pauseButton.addEventListener('click', function () {
+    if (audioElement.paused) {
+        audioElement.play();
+        pauseButton.textContent = "Pausar";
+    } else {
+        audioElement.pause();
+        pauseButton.textContent = "Reanudar";
+    }
+})
